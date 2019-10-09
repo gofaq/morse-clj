bf90aa12d5f942b162a9e84d387d39570ac1b514
Matsukiro Dev <matsukiro.dev@gmail.com>
2019-10-09 19:09:26 +0300

    initial coomit

---
+++ src/morse/core.clj
@@ -0,0 +1,75 @@
+(ns morse.core
+  (:require [clojure.set]
+            [clojure.string :as str]))
+
+(def alpha->morse {
+                  "a" ".-"
+                  "b" "-.."
+                  "c" "-.-."
+                  "d" "-.."
+                  "e" "."
+                  "f" "..-."
+                  "g" "--."
+                  "h" "...."
+                  "i" ".."
+                  "j" ".---"
+                  "k" "-.-"
+                  "l" ".-.."
+                  "m" "--"
+                  "n" "-."
+                  "o" "---"
+                  "p" ".--."
+                  "q" "--.-"
+                  "r" ".-."
+                  "s" "..."
+                  "t" "-"
+                  "u" "..-"
+                  "v" "...-"
+                  "w" ".--"
+                  "x" "-..-"
+                  "y" "-.--"
+                  "z" "--.."
+                  "1" ".----"
+                  "2" "..---"
+                  "3" "...--"
+                  "4" "....-"
+                  "5" "....."
+                  "6" "-...."
+                  "7" "--..."
+                  "8" "---.."
+                  "9" "----."
+                  "0" "----"
+                  })
+
+(def morse->alpha
+  (clojure.set/map-invert alpha->morse))
+
+(def alphabets
+  {:alpha alpha->morse
+   :morse morse->alpha})
+
+(defn current-alphabet
+  [type]
+  (get alphabets type))
+
+(defn chars-join
+  [type chars]
+  (cond
+    (= type :morse) (apply str chars)
+    (= type :alpha) (str/join " " chars)))
+
+(defn get-single-character
+  [alpha character]
+    (get alpha (str character)))
+
+(defn get-characters
+  [type characters]
+  (let [characters (cond
+                     (= type :morse) characters
+                     (= type :alpha) (str/join "" characters))]
+    (chars-join type (map #(get-single-character (current-alphabet type) %) characters))))
+
+(defn convert
+  [type phrase]
+  (get-characters type (str/split phrase #" ")))
+
---
+++ test/morse/core_test.clj
@@ -0,0 +1,26 @@
+(ns morse.core-test
+  (:require [clojure.test :refer :all]
+            [morse.core :refer :all]))
+
+(deftest morse-code-test
+  (testing "Testing structure and types of morse (&& morse revert) codes"
+           (is (map? alpha->morse))
+           (is (not (empty? alpha->morse)))
+           (is (map? morse->alpha))
+           (is (not (empty? morse->alpha))))
+
+  (testing "Testing single characters (morse => alphabet && alphabet => morse)"
+           (is (= (get-single-character (current-alphabet :alpha) "a") ".-"))
+           (is (= (get-single-character (current-alphabet :alpha) "1") ".----"))
+           (is (= (get-single-character (current-alphabet :morse) ".-") "a"))
+           (is (= (get-single-character (current-alphabet :morse) ".----") "1")))
+
+  (testing "Testing convert morse to alphabet"
+           (is (=  "sos"))
+           (is (= (convert :morse "-.. .- - -- .- -.") "batman"))
+           (is (= (convert :morse ".- .-. . .- ..... .----") "area51")))
+
+  (testing "Testing convert alphabet to morse"
+           (is (= (convert :alpha "sos") "... --- ..."))
+           (is (= (convert :alpha "batman") "-.. .- - -- .- -."))
+           (is (= (convert :alpha "area51") ".- .-. . .- ..... .----"))))
